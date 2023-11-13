ef281d5e6543f3bbf767fff4945f5e5b557e3933
igoncharov <igoncharov@flightpath3d.com>
2023-11-13 21:57:49 +0000

    feat: connect Montserrat font-famaly

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,14 @@ export default defineNuxtConfig({
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css?family=Montserrat',
+        },
+      ],
     },
   },
 
