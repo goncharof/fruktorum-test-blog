c012d1993dd4c7b45ab644bb12991cccf5b47079
igoncharov <igoncharov@flightpath3d.com>
2023-11-13 16:56:46 +0000

    feat: image fade component

---
+++ components/common/ImageFade.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const { src } = defineProps<{ src: string }>()
+const realsrc = ref('')
+const loaded = ref(false)
+
+onMounted(() => nextTick(() => realsrc.value = src),
+)
+</script>
+
+<template>
+  <transition name="fade">
+    <img
+      v-show="loaded"
+      alt=""
+      :src="realsrc"
+      @load="loaded = true"
+      @loadingComplete="loaded = true"
+    >
+  </transition>
+</template>
