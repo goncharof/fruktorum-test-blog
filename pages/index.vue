784c12581c4debbd1da9a60ba47886f3fc0d6a68
igoncharov <igoncharov@flightpath3d.com>
2023-11-13 15:20:37 +0000

    feat: index page - fetch data, set meta

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { data } = await useAsyncData<any>(
+  'list',
+  () => useBaseFetch('/', { method: 'GET', params: { path: '/' } }),
+)
+
+useHead({
+  meta: data.value.meta,
+})
+</script>
+
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
