c527fd92177a0e132d8eefc6186dbe6f7ece652f
igoncharov <igoncharov@flightpath3d.com>
2023-11-13 15:18:28 +0000

    feat: nuxt.config - nitro api proxy, base meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: ['@nuxt/ui'],
+
   css: [
     '~/node_modules/reset-css/reset.css',
     '~/assets/css/variables.css',
     '~/assets/css/global.css',
   ],
+
+  app: {
+    head: {
+      title: 'fruktorum-test-blog',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
+  },
+
+  // CORS
+  nitro: {
+    routeRules: {
+      '/proxy/**': { proxy: 'https://devtwit8.ru/api/v1/page/**' },
+    },
+  },
 })
