6cb5d80971b33db1247326142994f141381be4d1
igoncharov <igoncharov@flightpath3d.com>
2023-11-13 18:38:47 +0000

    feat: article page

---
+++ pages/[article].vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const { params: { article } } = useRoute()
+
+const { data, error } = await useAsyncData<any>(
+  `${article}`,
+  () => useBaseFetch('/', { method: 'GET', params: { path: `/${article}` } }),
+)
+
+if (error.value)
+  throw createError({ statusCode: 404 })
+
+useHead({
+  meta: data.value.meta,
+})
+
+const blocks: any = {
+  article_intro_block: defineAsyncComponent(() => import('@/components/article/IntroBlock.vue')),
+  text_block: defineAsyncComponent(() => import('@/components/article/TextBlock.vue')),
+}
+
+console.log(data.value.body)
+</script>
+
+<template>
+  <div>
+    <component :is="blocks[block.type] ?? null" v-for="block in data.body" :key="block.id" :data="block.data" />
+  </div>
+</template>
