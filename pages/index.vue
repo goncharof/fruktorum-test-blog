dea0c80388a88c1576b7678f1ed968d587ab7ae2
yolo <igoncharov@flightpath3d.com>
2023-11-14 14:30:19 +0000

    feat: index page - ts
    also fixed meta tags and handle http errors

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
-const { data } = await useAsyncData<any>(
-  'list',
-  () => useBaseFetch('/', { method: 'GET', params: { path: '/' } }),
-)
-
-useHead({
-  meta: data.value.meta,
+const { data, error } = await useFetch<{
+  id: string
+  body: [{ data: { articles: { image: string; title: string; link: string }[] } }]
+  meta: { title: string; description: string }
+}>('/proxy/', {
+  pick: ['body', 'meta'],
+  params: { path: '/' },
+  deep: false,
+  key: 'index',
+  method: 'GET',
 })
+
+if (error.value || !data.value)
+  throw createError({ statusCode: 404 })
+
+useHead({ ...data.value.meta })
 </script>
 
 <template>
   <div class="card-container">
     <ArticleCard
-      v-for="article in data.body[0].data.articles"
+      v-for="article in data!.body[0].data.articles"
       :key="article.link"
       :image-url="article.image"
       :card-text="article.title"
@@ -27,7 +35,6 @@ useHead({
   flex-wrap: wrap;
   justify-content: space-between;
 }
-
 .card {
   min-width: 30%;
   margin-bottom: 20px;
